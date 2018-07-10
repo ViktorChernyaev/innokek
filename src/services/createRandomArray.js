52971740e7b91e9bec4afb7056db5fc11c44054a
ViktorChernyaev <bagunok18@gmail.com>
2018-07-10 23:56:10 +0300

    Added generators

--- src/services/createRandomArray.js
+++ src/services/createRandomArray.js
@@ -18,8 +18,26 @@ const nameTemplates = [
     "massa",
     "natoque"
 ];
+const randomName = () => nameTemplates[randomMinMax(0, 15)];
+const randomKeysCount = () => randomMinMax(1, 10);
 
-function randomName() {
-    return nameTemplates[randomMinMax(0, 15)];
-};
+function* objectGenerator(keysCount) {
+    let i = 0;
+    while (i < keysCount) {
+        i++;
+        yield { [randomName()]: randomValue() };
+    }
+}
+
+function* arrayGenerator(k = 1000) {
+    let i = 0;
+    while (i < k) {
+        i++;
+        yield Object.assign({}, ...objectGenerator(randomKeysCount()));
+    }
+}
+
+export default function generate(length) {
+    return Array.from(arrayGenerator(length));
+}
 
