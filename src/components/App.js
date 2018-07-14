8464d3c6e0ecd1316d8091b46b4016cb79eab3c5
ViktorChernyaev <bagunok18@gmail.com>
2018-07-14 13:32:45 +0300

    Add router, nav, empty page for todolist

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect, NavLink } from "react-router-dom";
 import FilterPanel from "components/filterPanel";
+import TodoList from "components/todoList";
 
 const App = () => (
-    <div className="page-wrapper">
-        <FilterPanel />
-    </div>
+    <BrowserRouter>
+        <div className="page-wrapper">
+            <nav className="panel page-nav">
+                <NavLink className="page-nav__link" activeClassName="page-nav__link--active" to="/todo">
+                    Список дел
+                </NavLink>
+                <NavLink className="page-nav__link" activeClassName="page-nav__link--active" to="/filter">
+                    Фильтр
+                </NavLink>
+            </nav>
+            <Switch>
+                <Route exact path="/" render={() => <Redirect to="/todo" />} />
+                <Route path="/todo" component={TodoList} />
+                <Route path="/filter" component={FilterPanel} />
+            </Switch>
+        </div>
+    </BrowserRouter>
 );
 
 export default App;
